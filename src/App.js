7bfb11dc89d6d7c1e4276e6b4e92f17e565277c4
Lakshya Choudhary <luckshyachoudhary@gmail.com>
2019-10-24 12:28:54 +0530

    working eg after grid

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import * as firebase from 'firebase'
 import {DB_CONFIG} from './firebase.js'
 import Booking from './booking'
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
 class compToPrint extends React.Component{
   constructor(props){
@@ -11,7 +14,26 @@ class compToPrint extends React.Component{
       this.db = firebase.firestore();
       this.bookingsRef = this.db.collection('bookings');
       this.state = {
-        bookings : []
+        bookings : [],
+        columnDefs: [
+          {
+            headerName: "Checked In", field: "checkedIn",filter:true,editable:true,
+            cellRenderer: params => {
+              return `<input type='checkbox' ${params.value ? 'checked' : ''} />`;
+          }
+          },
+          {
+            headerName: "Room No", field: "rId",sortable: true,filter:true,editable:true,
+          },
+          {
+          headerName: "ID", field: "bId",sortable: true,filter:true,
+          cellRenderer: (cellValue) => 
+              `<a href="/booking/${cellValue.value}" >${cellValue.value}</a>`
+        }, {
+          headerName: "Name", field: "name",sortable: true,filter:true
+        }, {
+          headerName: "Status", field: "status",sortable: true ,filter:true,editable:true
+        }]
       }
   }
 
@@ -28,16 +50,31 @@ class compToPrint extends React.Component{
     })
   }
 
-  render(){
-    return(
-      <div class="list-group">
-      {this.state.bookings.map((booking)=>{
-        return(
-        <Booking name={booking.name} />  
-      )})}
+  onButtonClick = e => {
+    const selectedNodes = this.gridApi.getEditingCells();
+    const selectedData = selectedNodes.map( node => node.data )
+    const selectedDataStringPresentation = selectedData.map( node => node.make + ' ' + node.model).join(', ')
+    alert(`Selected nodes: ${selectedDataStringPresentation}`)
+    }
+
+  render() {
+    return (
+      <div 
+        className="ag-theme-balham"
+        style={{ 
+        height: '550px', 
+        width: '1200px' }} 
+      >
+         <button onClick={this.onButtonClick}>Get selected rows</button>
+        <AgGridReact
+          onGridReady={ params => this.gridApi = params.api }
+          pagination = {true}
+          columnDefs={this.state.columnDefs}
+          rowData={this.state.bookings}>
+        </AgGridReact>
       </div>
-    )  
-}
+    );
+  }
 
   
 }
